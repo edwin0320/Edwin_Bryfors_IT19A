3c8143970a0fdac8651be2c67e6f5aceabb004d8
edwin0320 <70261750+edwin0320@users.noreply.github.com>
2020-09-08 09:40:48 +0200

    spara

--- variabler_io/index.js
+++ variabler_io/index.js
@@ -4,3 +4,14 @@
 let name = "Edwin"
 console.log(`Hej ${name}`)
 
+
+
+let tal1 = 3
+let tal2 = 4
+let tal3 = 5
+
+medelvärde = (tal1+tal2+tal3)/3
+
+console.log(`${tal1}+${tal2}+${tal3}/3 =${medelvärde}`) 
+
+let langd = promt("ange värdet för rektangelns längd")
