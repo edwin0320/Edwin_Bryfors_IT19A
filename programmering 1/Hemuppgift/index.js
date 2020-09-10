ef8e5537df79db2d02d480b0f3f3f099b87076f5
edwin0320 <70261750+edwin0320@users.noreply.github.com>
2020-09-10 09:30:52 +0200

    hej

---
+++ programmering 1/Hemuppgift/index.js
@@ -0,0 +1,14 @@
+
+
+
+let name = prompt("Vad heter du?")
+let age = prompt("Hur gammal är du?")
+let adress = prompt("Vad är din adress?")
+let postnummer = prompt("vad är ditt postnummer?")
+let stad = prompt("Vilen stad bor du i?")
+let telefonnummer = prompt("vad är ditt telefonnummer?")
+
+console.log(`Namn: ${name}`)
+console.log(`Ålder: ${age}`)
+console.log(`Adress: ${adress} ${postnummer} ${stad}`)
+console.log(`Telefon ${telefonnummer}`)
---
+++ programmering 1/variabler_io/index.js
@@ -0,0 +1,25 @@
+// skapa en vaiabel name och tilldela det ditt namn
+
+// skriv ut "hej namn"
+let name = "Edwin"
+console.log(`Hej ${name}`)
+
+
+
+let tal1 = 3
+let tal2 = 4
+let tal3 = 5
+
+medelvärde = (tal1+tal2+tal3)/3
+
+console.log(`${tal1}+${tal2}+${tal3}/3 =${medelvärde}`) 
+
+let langd = prompt("Ange längden på rektangeln")
+let bredd = prompt("Ange bredden på rektangeln")
+
+let area = bredd*langd
+
+console.log(`Bredden på rektangeln är ${bredd}`)
+console.log(`Längden på rektangeln är ${langd}`)
+console.log(`Arean av rektangeln är ${area}`)
+
