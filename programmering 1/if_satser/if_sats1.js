5e4b650b877f76ce7a9af3da9d4b2deed1278040
edwin0320 <70261750+edwin0320@users.noreply.github.com>
2020-09-15 09:37:29 +0200

    prog 1

---
+++ programmering 1/if_satser/if_sats1.js
@@ -0,0 +1,20 @@
+// modulusoperator
+console.log(`${5%2}`)
+
+
+
+// avgöra om talet är jämnt eller udda
+
+// tal%2 är 0: jämnt
+// tal%2 är 1: udda
+
+let tal=prompt ("skriv ett tal")
+
+if (tal%2 === ){
+    console.log("postivt")
+} else {
+    console.log("udda")
+}
+
+
+
