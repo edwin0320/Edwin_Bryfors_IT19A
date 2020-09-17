e62b54ad7f86c0d89d0899ddd8eb2853769f4a7e
edwin0320 <70261750+edwin0320@users.noreply.github.com>
2020-09-17 09:53:09 +0200

    ja

---
+++ programmering 1/if_satser/if_sats2.js
@@ -0,0 +1,29 @@
+
+
+let age = prompt("Hur gammal är du")
+
+if (age >= 15){
+    console.log("Du får kolla på en film med 15 års gräns")
+} else {
+    console.log("Du får inte kolla på en film med 15 års gräns")
+}
+
+if (age >= 7){
+    console.log("Du får kolla på en film med 7 års gräns")
+} else {
+    console.log("Du får inte kolla på en film med 7 års gräns")
+}
+
+if (age >= 18){
+    console.log("Du får kolla på en film med 18 års gräns")
+} else {
+    console.log("Du får inte kolla på en film med 18 års gräns")
+}
+
+let antal = prompt ("Hur många gånger åker du spårvagn i månaden")
+
+if (antal >=28){
+    console.log("Du åker spårvagn mer än 28 gånger och det är värt att köpa månads kort")
+} else{
+    console.log ("Du åker spårvafgn mindre än 28 gånger i månaden så det är inte värt att köpa månadskort")
+}
