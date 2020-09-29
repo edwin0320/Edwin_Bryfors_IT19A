a9c83bafda01d85368cab45f37f1843042fd75f0
edwin0320 <70261750+edwin0320@users.noreply.github.com>
2020-09-29 09:44:54 +0200

    hej

--- programmering 1/For_sats/for.js
+++ programmering 1/For_sats/for.js
@@ -2,7 +2,7 @@ console.log("Hej")
 let p_talserie1 = document.querySelector("#talserie1")
 let p_talserie2 = document.querySelector("#talserie2")
 let p_summa = document.querySelector("#summa")
-
+let p_klass = document.querySelector("#Klasslista")
 
 for (let i = 0; i<=10; i++){
 p_talserie1.innerHTML += `${i}`
@@ -13,7 +13,7 @@ for (let j=10; j>0; j--){
 }
 
 for (let i = 0; i<=10; i++){
-    console.log(`${i}. I will not write all iver the walls`)
+    console.log(`${i}. I will not write all over the walls`)
 }
 
 // Vill räkna ut summan 1+2+3+...+8+9+10
@@ -22,4 +22,17 @@ for (let i=0; i<=10; i++){
     summa += i
 }
 
-console.log(summa)
+p_summa.innerHTML += `1 + 2 + 3 +...+ 9+10 = ${summa}`
+
+
+let elever = ["Kokchun", "Henrick", "Thomas", "David", "Tatiana"]
+
+p_klass.innerHTML = " Klass IT19A innehåller följande elever: <br/>"
+
+console.log(elever.length)
+
+for (let k = 0; k < elever.length; k++){
+    p_klass.innerHTML += `${k+1}. ${elever[k]} <br/>`
+
+}
+
