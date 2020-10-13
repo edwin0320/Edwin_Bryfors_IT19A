3d28cd624217693d149628aeb68b03fbd45a8dc6
edwin0320 <70261750+edwin0320@users.noreply.github.com>
2020-10-13 09:39:03 +0200

    funktioner lektion 1

---
+++ programmering 1/Funktioner/funktioner1.js
@@ -0,0 +1,40 @@
+let namn = "Edwin"
+
+function sayHello(){
+    let alder = 17
+    console.log(`Hej ${namn}, du är ${alder} år gammal`)
+}
+
+
+sayHello()
+sayHello()
+//console.log(`Min ålder är ${alder}`)
+
+
+function addera(tal1, tal2){
+    let summa = tal1+tal2
+return summa
+}
+console.log(`2+5=${addera(2,5)}`)
+
+
+function subtration(tal1, tal2){
+    let differens = tal1-tal2
+    return differens 
+}
+console.log(`3-8=${subtration(3,8)}`)
+
+
+function multiplikation(tal1, tal2){
+    let summa = tal1*tal2
+    return summa 
+}
+console.log(`6*5=${multiplikation(6,5)}`)
+
+
+function divition(tal1, tal2){
+    let kvot = tal1/tal2
+    return kvot 
+}
+console.log(`12/3=${divition(12,3)}`)
+
