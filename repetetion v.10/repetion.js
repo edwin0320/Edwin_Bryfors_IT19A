93c8b22458ae6d99733d5045268804694284fe0c
edwin0320 <70261750+edwin0320@users.noreply.github.com>
2021-03-09 09:06:32 +0100

    save

---
+++ repetetion v.10/repetion.js
@@ -0,0 +1,21 @@
+
+
+
+let namn = "Edwin"
+let ålder = 17
+
+console.log(`hej ${namn}, du är ${ålder} år gammal`)
+
+
+if (ålder >= 18){
+console.log("Du är vuxen")
+} else {
+    console.log("Du är barn")
+}
+
+
+for (let i =0; i < 20; i++){
+    if (i % 2 == 0){
+        console.log(i);
+    }
+  }
