27904a58724ee1312e21754169f5a98daae31d68
edwin0320 <70261750+edwin0320@users.noreply.github.com>
2021-04-27 14:56:41 +0200

    save

---
+++ öva.js
@@ -0,0 +1,10 @@
+let tal1=prompt("Skriv ett tal: ")
+
+let tal2=prompt("Skriv ett till tal: ")
+
+if (tal1+tal2==10){
+    console.log("Det är ett kompistal")
+}
+else {
+    console.log("Det är inte ett kompistal")
+}
